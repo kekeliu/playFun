f9344463bdc8cb6a03a94953f20e7e33657b8123
kekeliu <1042311019@qq.com>
2013-11-26 14:29:48 +0800

    write more about the secure module

--- playFun/app/controllers/Security.java
+++ playFun/app/controllers/Security.java
@@ -7,4 +7,12 @@ public class Security extends Secure.Security {
 	static boolean authenicate(String username, String password){
 		return User.connect(username, password) != null;
 	}
+	
+	static void onDisconnected(){
+		Application.index();
+	}
+	
+	static void onAuthenticated(){
+		Admin.index();
+	}
 }
